ca85f713b0243d63f2e31ee06074d387234b15f0
HyBeard <Kaptsevich.i@gmail.com>
2020-02-26 19:57:51 +0300

    feat: create index-pages programmatically for every lang

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,66 +1,56 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
+import { useTranslation } from 'react-i18next';
+
+// TODO: add styles, config rhythm
+// import { rhythm } from '../utils/typography';
 
 import Layout from '../components/Layout';
-import { rhythm } from '../utils/typography';
+import LangSwitcher from '../components/LangSwitcher';
 
-const BlogIndex = ({ data /* location */ }) => {
-  // const siteTitle = data.site.siteMetadata.title;
-  const writersFiles = data.allMarkdownRemark.edges;
+const MainPage = ({ data, pageContext: { pageLang } }) => {
+  const { t } = useTranslation();
+  const {
+    allMarkdownRemark: { edges },
+  } = data;
 
+  // TODO: create lang filtering by graphQl-cli
+  const aboutPortal = edges.find(
+    ({ node }) =>
+      node.frontmatter.dataKey === 'aboutPortal' && node.frontmatter.contentLang === pageLang,
+  );
+
+  // FIXME: find safe method to create page from markdown
   return (
     <Layout>
-      {writersFiles.map(({ node }) => {
-        const title = node.frontmatter.name;
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        );
-      })}
+      <LangSwitcher />
+      <div dangerouslySetInnerHTML={{ __html: aboutPortal.node.html }} />
+      <h1>{t('artistsPageButton')}</h1>
+
+      <button type="button" onClick={() => console.log(aboutPortal)}>
+        console query
+      </button>
     </Layout>
   );
 };
 
-export default BlogIndex;
+export default MainPage;
 
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(filter: { frontmatter: { type: { eq: "writer-data" } } }) {
+    allMarkdownRemark {
       edges {
         node {
-          id
-          excerpt
-          fields {
-            slug
-          }
           frontmatter {
+            dataKey
+            contentLang
             name
-            video
-            works
-            yearsOfLife
             photo {
               absolutePath
             }
+            yearsOfLife
           }
+          html
         }
       }
     }
