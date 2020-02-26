c85bfd8faa806c021ffba0ab2cb2c5782bf3229a
nextdesusu <nextdesusu@gmail.com>
2020-02-26 21:57:45 +0400

    feat: finish photogallery component

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
-const PhotoGallery = ({ data }) => {
+const PhotoGallery = ({ slides }) => {
   return (
-    <>
-      {data.map((item) => (
-        <div>{item}</div>
-      ))}
-    </>
+    <Carousel>
+      {slides.map((slide, index) => {
+        const slideKey = `slide: ${index}`;
+        const { imgSrc, title, text } = slide;
+        return (
+          <Carousel.Item key={slideKey}>
+            {imgSrc && <img src={imgSrc} className="d-block w-100" alt={slideKey} />}
+            <Carousel.Caption>
+              <h3>{title}</h3>
+              <p>{text}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        );
+      })}
+    </Carousel>
   );
 };
 
