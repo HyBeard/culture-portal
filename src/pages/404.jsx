7e129178472153d29324fe2be36d98a3f3d90075
HyBeard <Kaptsevich.i@gmail.com>
2020-02-26 17:34:31 +0300

    refactor: return back 404.jsx to the root to have common error-page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from '../../components/Blog/layout';
-import SEO from '../../components/Blog/seo';
+import Layout from '../components/Blog/layout';
+import SEO from '../components/Blog/seo';
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title;
