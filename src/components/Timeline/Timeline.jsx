920aceca85fd8a553a5e0ea7a40e5e4f74cf6597
nextdesusu <nextdesusu@gmail.com>
2020-02-28 22:45:35 +0400

    init: create initial files for timeline

---
+++ src/components/Timeline/Timeline.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Timeline = ({ data }) => {
+  console.log(data);
+  return <section />;
+};
+
+export default Timeline;
---
+++ src/components/Timeline/index.js
@@ -0,0 +1,3 @@
+import './Timeline.scss';
+
+export { default } from './Timeline';
