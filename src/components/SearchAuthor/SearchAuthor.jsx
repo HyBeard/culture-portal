b0f74263707748dcb0a8ff9dc30000dffd5842c1
nextdesusu <nextdesusu@gmail.com>
2020-02-28 16:32:53 +0400

    feat: done SearchAuthor also changed parser to babel

--- src/components/SearchAuthor/SearchAuthor.jsx
+++ src/components/SearchAuthor/SearchAuthor.jsx
@@ -1,54 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Table from 'react-bootstrap/Table';
+import { Link } from 'gatsby';
 
-class SearchAuthor extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            query: '',
-            authorsHandled: this.props.authorsList,
-        };
-    }
+class SearchAuthor extends Component {
+  constructor(props) {
+    super(props);
+    const { authors } = this.props;
+    this.state = {
+      query: '',
+      authorsHandled: authors,
+    };
+  }
 
-    changeHandler = ({ target: {value} }) => {
-        const { authorsList } = this.props;
-        const newList = authorsList.filter((item) => {
-            const indexOfNonFinded = -1;
-            const checkName = item.name.toLowerCase();
-            const checkValue = value.toLowerCase();
-            const index = checkName.indexOf(checkValue);
-            return index !== indexOfNonFinded;
-        });
-        this.setState({ query: value, authorsHandled: newList });
-    }
+  changeHandler = ({ target: { value } }) => {
+    const { authors } = this.props;
+    const newList = authors.filter((item) => {
+      const indexOfNonFinded = -1;
+      const checkName = item.name.toLowerCase();
+      const checkValue = value.toLowerCase();
+      const index = checkName.indexOf(checkValue);
+      return index !== indexOfNonFinded;
+    });
+    this.setState({ query: value, authorsHandled: newList });
+  };
 
-    render(){
-        const { child, title, placeholder } = this.props;
-        console.log(this.state.authorsHandled);
-        // Get input trhough child
-        /*
-        <SearchAuthor authorsList={[
-          {name: '1123'},
-          {name: 'anton'},
-          {name: 'jora'},
-
-        ]}/>
-        */
-        return (
-            <div>
-                <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="basic-addon1" />
-                    <FormControl
-                        placeholder={placeholder}
-                        onChange={this.changeHandler}
-                    />
-                </InputGroup.Prepend>
-                </InputGroup>
-            </div>
-        )
-    }
+  render() {
+    const { placeholder, children } = this.props;
+    const { authorsHandled, query } = this.state;
+    return (
+      <div className="search-author">
+        {children}
+        <InputGroup className="mb-3">
+          <FormControl placeholder={placeholder} onChange={this.changeHandler} value={query} />
+        </InputGroup>
+        <Table responsive>
+          <tbody>
+            {authorsHandled.map((author, index) => {
+              const key = `author: ${index}`;
+              const { name, to } = author;
+              return (
+                <tr key={key}>
+                  <td>
+                    <Link to={to || '#'}>{name}</Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default SearchAuthor;
