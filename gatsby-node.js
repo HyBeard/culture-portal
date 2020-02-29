e08b0b44cb4b01f1204c569778d0087740397e99
HyBeard <Kaptsevich.i@gmail.com>
2020-02-29 21:03:29 +0300

    feat: remove the blog from the site content

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,10 @@ const { createFilePath } = require('gatsby-source-filesystem');
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
-  const blogPost = path.resolve('./src/templates/blog-post.jsx');
   const mainPage = path.resolve('./src/templates/main-page.jsx');
   const authorPage = path.resolve('./src/templates/author-page.jsx');
   const errorPage = path.resolve('./src/pages/404.jsx');
 
-  // FIXME: change data grabbing (graphQl-cli with variables)
   const dataQuery = await graphql(`
     {
       site {
@@ -18,22 +16,6 @@ exports.createPages = async ({ graphql, actions }) => {
           langs {
             defaultLangKey
             list
-          }
-        }
-      }
-      allMarkdownRemark(
-        sort: { fields: [frontmatter___date], order: DESC }
-        limit: 1000
-        filter: { frontmatter: { type: { eq: "post-data" } } }
-      ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-            }
           }
         }
       }
@@ -54,22 +36,6 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   const langsList = dataQuery.data.site.siteMetadata.langs.list;
-  const posts = dataQuery.data.allMarkdownRemark.edges;
-
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-    const next = index === 0 ? null : posts[index - 1].node;
-
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    });
-  });
 
   langsList.forEach((lng) => {
     createPage({
