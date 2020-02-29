94bec0c336f69bea06a4e91f096292ea0229a3c2
nextdesusu <nextdesusu@gmail.com>
2020-02-29 20:18:00 +0400

    feat: finish timeline component

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import TimelineItem from '../TimelineItem';
 
-const Timeline = ({ data }) => {
-  console.log(data);
-  return <section />;
+const Timeline = ({ lines }) => {
+  return (
+    <section className="timeline">
+      {lines.map((item, index) => {
+        const [date, description] = item;
+        const key = `timeline: ${index}`;
+        const isRight = index % 2 !== 0;
+        return (
+          <TimelineItem isRight={isRight} key={key} date={date}>
+            {description}
+          </TimelineItem>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Timeline;
--- src/components/TimelineItem/TimelineItem.jsx
+++ src/components/TimelineItem/TimelineItem.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-const TimelineItem = ({ date, text }) => {
+const TimelineItem = ({ date, children, isRight }) => {
+  const orderRight = 3;
+  const orderLeft = 1;
   return (
-    <Card border="secondary" style={{ width: '18rem' }}>
-      <Card.Header>{date}</Card.Header>
-      <Card.Body>
-        <Card.Text>{text}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="timelineItem">
+      <Card
+        border="secondary"
+        style={{
+          position: 'relative',
+          order: isRight ? orderRight : orderLeft,
+          left: isRight ? '-0.5em' : '0.5em',
+          zIndex: 10,
+        }}
+      >
+        <Card.Header>{date}</Card.Header>
+        <Card.Body>
+          <Card.Text>{children}</Card.Text>
+        </Card.Body>
+      </Card>
+      <div className="timelineItem-line" />
+      <div style={{ order: isRight ? orderLeft : orderRight }} />
+    </div>
   );
 };
 
