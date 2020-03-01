6291dbc118abdce25ce0567dcc91ffa6e243a0aa
HyBeard <Kaptsevich.i@gmail.com>
2020-03-01 19:08:28 +0300

    feat: init i18n with correct language

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -7,26 +7,28 @@ import glossaryEN from '../../content/glossary/en.json';
 import glossaryRU from '../../content/glossary/ru.json';
 import glossaryBE from '../../content/glossary/be.json';
 
-// the translations
 const resources = {
   en: { translation: glossaryEN },
   ru: { translation: glossaryRU },
   be: { translation: glossaryBE },
 };
 
+i18n.on('initialized', () => {
+  const actualLang = window.location.pathname.split('/')[1];
+
+  i18n.changeLanguage(actualLang);
+});
+
 i18n
   .use(detector)
   .use(backend)
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .init({
     resources,
     lng: 'en',
+    fallbackLng: 'en',
 
-    keySeparator: false, // we do not use keys in form messages.welcome
-
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
+    keySeparator: false,
   });
 
 export default i18n;
