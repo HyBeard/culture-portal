5c04e22c0695420a0a7adf061b2af8f3554ea878
Dmitry Radkovets <dmitry.radkovets@gmail.com>
2020-03-03 13:48:15 +0300

    feat: create functional table list component

--- src/components/MainDifficultiesList/MainDifficultiesList.jsx
+++ src/components/MainDifficultiesList/MainDifficultiesList.jsx
@@ -4,7 +4,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 const MainDifficultiesList = () => {
   return (
     <ListGroup variant="flush">
-      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item id="lishka">Cras justo odio</ListGroup.Item>
       <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
       <ListGroup.Item>Morbi leo risus</ListGroup.Item>
       <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
--- src/pages/worklog.jsx
+++ src/pages/worklog.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { withTranslation } from 'react-i18next';
 
 import Layout from '../components/Layout';
-import FunctionalTable from '../components/FunctionalTable';
+import FunctionalTableList from '../components/FunctionalTableList';
 import MainDifficultiesList from '../components/MainDifficultiesList';
 
-const Worklog = ({ t }) => {
+const Worklog = ({ data, t }) => {
+  const usersInfo = data.markdownRemark.frontmatter.team;
+
   return (
     <Layout>
-      <h2>{t('worklogTitle')}</h2>
-      <h3>Таблица</h3>
-      <FunctionalTable />
-      <h3>Главные трудности при разработке</h3>
-      <MainDifficultiesList />
+      <h2 style={{ textAlign: 'center' }}>{t('worklogTitle')}</h2>
+      <section className="section-functional-table">
+        <FunctionalTableList data={usersInfo} />
+      </section>
+      <section className="section-main-difficulties">
+        <h3>{t('mainDifficultiesTitle')}</h3>
+        <MainDifficultiesList />
+      </section>
     </Layout>
   );
 };
 
 export default withTranslation()(Worklog);
+
+export const pageQuery = graphql`
+  {
+    markdownRemark(frontmatter: { contentLang: { eq: "ru" }, dataKey: { eq: "teamInfo" } }) {
+      frontmatter {
+        team {
+          worklog {
+            feature
+            spentTime
+          }
+          name
+        }
+      }
+    }
+  }
+`;
